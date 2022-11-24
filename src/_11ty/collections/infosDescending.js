226d082bd2c1e5aaf982958947d71d496f77afda
Jens Grochtdreis <jens@grochtdreis.de>
2022-11-24 21:55:48 +0100

    collections entfernt

--- src/_11ty/collections/infosDescending.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(collection) {
-  return collection
-    .getFilteredByGlob("./src/content/infos/*.md").sort((a,b) => {
-      if (a.data.title < b.data.title) return -1;
-      else if (a.data.title > b.data.title) return 1;
-      else return 0;
-    })
-};
