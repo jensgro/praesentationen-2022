cf393ea7a1c67a81ce4ec987d55e9ae925145795
Jens Grochtdreis <jens@grochtdreis.de>
2022-11-24 23:05:22 +0100

    unnötiges aus eleventy.js entfernt

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,4 @@
 const fs = require("fs");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
@@ -9,18 +7,12 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlDateString = require("./src/_11ty/filters/date.js").htmlDateString;
 const head = require("./src/_11ty/filters/head.js");
 
-// collections
-const infosDescending = require("./src/_11ty/collections/infosDescending.js");
-const tagList = require("./src/_11ty/collections/tagList.js");
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/assets/scss/");
 
   eleventyConfig.addFilter("htmlDateString", htmlDateString);
   eleventyConfig.addFilter("head", head);
 
-  eleventyConfig.addCollection("infosDescending", infosDescending);
-  eleventyConfig.addCollection("tagList", tagList);
 
   // keine Ahnung, was das tut :-)
   eleventyConfig.setDataDeepMerge(true);
